11166c6b813a88317ed803ebd6780af9eb54f8c7
Bolun Andrei <telfagor@gmail.com>
2021-11-15 21:38:24 +0200

    3. Перегрузить метод max для трех чисел [#122644 #228582]

--- src/main/java/ru/job4j/tracker/Max.java
+++ src/main/java/ru/job4j/tracker/Max.java
@@ -1,15 +1,15 @@
 package ru.job4j.tracker;
 
 public class Max {
-    public double add(double first, double second) {
-        return first + second;
+    public double max(double first, double second) {
+        return Math.max(first, second);
     }
 
-    public double add(double first, double second, double third) {
-        return add(first, add(second, third));
+    public double max(double first, double second, double third) {
+        return max(first, max(second, third));
     }
 
-    public double add(double first, double second, double third, double fourth) {
-        return add(first, add(second, third, fourth));
+    public double max(double first, double second, double third, double fourth) {
+        return max(first, max(second, third, fourth));
     }
 }
--- src/test/java/ru/job4j/tracker/MaxTest.java
+++ src/test/java/ru/job4j/tracker/MaxTest.java
@@ -6,32 +6,41 @@ import static org.junit.Assert.*;
 public class MaxTest {
 
     @Test
-    public void whenAddHasTwoParameters() {
+    public void whenMaxHasTwoParameters() {
         double first = 5.0;
-        double second = 5.0;
-        double rsl = new Max().add(first, second);
-        double expected = 10.0;
-        assertEquals(expected, rsl, 0.01);
+        double second = 6.0;
+        double rsl = new Max().max(first, second);
+        double expected = 6.0;
+        assertEquals(expected, rsl, 0.1);
     }
 
     @Test
-    public void whenAddHasThreeParameters() {
-        double first = 5.0;
-        double second = 5.0;
-        double third = 5.0;
-        double rsl = new Max().add(first, second, third);
-        double expected = 15.0;
-        assertEquals(expected, rsl, 0.01);
+    public void whenMaxHasTwoEqualParameters() {
+        double first = 0.0;
+        double second = 0.0;
+        double rsl = new Max().max(first, second);
+        double expected = 0.0;
+        assertEquals(expected, rsl, 0.1);
     }
 
     @Test
-    public void whenAddHasFourthParameters() {
+    public void whenMaxHasThreeParameters() {
         double first = 5.0;
-        double second = 5.0;
-        double third = 5.0;
-        double fourth = 5.0;
-        double rsl = new Max().add(first, second, third, fourth);
-        double expected = 20.0;
-        assertEquals(expected, rsl, 0.01);
+        double second = 6.0;
+        double third = 7.0;
+        double rsl = new Max().max(first, second, third);
+        double expected = 7.0;
+        assertEquals(expected, rsl, 0.1);
+    }
+
+    @Test
+    public void whenMaxHasFourthParameters() {
+        double first = 5.0;
+        double second = 6.0;
+        double third = 7.0;
+        double fourth = 8.0;
+        double rsl = new Max().max(first, second, third, fourth);
+        double expected = 8.0;
+        assertEquals(expected, rsl, 0.1);
     }
 }

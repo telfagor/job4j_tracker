0b5146c5cc6cfbe34c1b253816fdab163bbedf5e
Bolun Andrei <telfagor@gmail.com>
2021-11-15 19:50:45 +0200

    1.8. Состояние объекта [#167388 #226961]

--- src/main/java/ru/job4j/tracker/Battery.java
+++ src/main/java/ru/job4j/tracker/Battery.java
@@ -9,7 +9,7 @@ public class Battery {
 
     public void exchange(Battery another) {
         another.load += load;
-        this.load -= this.load;
+        this.load = 0;
     }
 
     public int getLoad() {

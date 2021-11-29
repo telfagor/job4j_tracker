3f4aa49d21c67039f81d857aee16927c29280ac7
Bolun Andrei <telfagor@gmail.com>
2021-11-29 22:01:19 +0200

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item i = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = i.getCreated().format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+    }
+}

07a464c4edf19018d0c28d0350094597cad9c785
Bolun Andrei <telfagor@gmail.com>
2021-12-07 21:09:11 +0200

    4. Сравнение моделей. Метод equals [#174893]

---
+++ src/test/java/pojo/LicenseTest.java
@@ -0,0 +1,19 @@
+package pojo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+public class LicenseTest {
+
+    @Test
+    public void eqName() {
+        License first = new License();
+        first.setCode("audio");
+        License second = new License();
+        second.setCode("audio");
+        assertThat(first, is(second));
+    }
+}

949be9d5e948589fbdd71be97f4f03698c96cab1
Bolun Andrei <telfagor@gmail.com>
2021-12-13 23:16:23 +0200

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,29 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item rsl = null;
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                items[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] array = new Item[size];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                array[index] = items[i];
+                index++;
+            }
+        }
+        return Arrays.copyOf(array, index);
     }
 }

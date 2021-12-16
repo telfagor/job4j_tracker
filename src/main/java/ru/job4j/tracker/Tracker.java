537563e58b8030816574d856c4b7dccc37646e82
Bolun Andrei <telfagor@gmail.com>
2021-12-16 21:33:09 +0200

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -44,6 +44,17 @@ public class Tracker {
         return true;
     }
 
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        }
+        System.arraycopy(items, index + 1, items, index, size - index - 1);
+        items[size - 1] = null;
+        size--;
+        return true;
+    }
+
     private int indexOf(int id) {
         int rsl = -1;
         for (int i = 0; i < size; i++) {

a63118e816abdde6ec469db2c92b365aadf45be8
Bolun Andrei <telfagor@gmail.com>
2021-12-30 21:39:46 +0200

    2. Scanner и чтение числа из консоли. [#179518]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -3,25 +3,49 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int inputControl(String player, int limit) {
+        String message;
+        if (limit == 3) {
+            message = player + " can take 1 to 3 matches";
+        } else if (limit == 2) {
+            message = player + " can take 1 or 2 matches";
+        } else {
+            message = player + " can take 1 match";
+        }
+        int matches;
+        do {
+            System.out.println(message);
+            matches = Integer.parseInt(scanner.nextLine());
+        } while (matches < 1 || matches > limit);
+        return matches;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Игра 11.");
+        System.out.println("The game 11");
         boolean turn = true;
         int count = 11;
         while (count > 0) {
-            String player = turn ? "Первый игрок" : "Второй игрок";
-            System.out.println(player + " введите число от 1 до 3:");
-            int matches = scanner.nextInt();
+            System.out.println("On the table are " + count + " matches");
+            String player = turn ? "First player" : "Second player";
+            int matches;
+            if (count >= 3) {
+                matches = inputControl(player, 3);
+            } else if (count == 2) {
+                matches = inputControl(player, 2);
+            } else {
+                matches = inputControl(player, 1);
+            }
             count -= matches;
-            System.out.println("На столе осталось " + (count != 1 ? count
-                    + " спичек " : count + " спичка"));
             turn = !turn;
         }
         scanner.close();
         if (turn) {
-            System.out.println("Выиграл второй игрок");
+            System.out.println("The second player won");
         } else {
-            System.out.println("Выиграл первый игрок");
+            System.out.println("The first player won");
         }
     }
 }
+

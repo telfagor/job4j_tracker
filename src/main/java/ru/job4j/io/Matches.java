bbd27e8711f8c6933d6761dd695e769d749f1a6f
Bolun Andrei <telfagor@gmail.com>
2022-01-03 20:37:24 +0200

    2. Scanner и чтение числа из консоли. [#179518]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -3,42 +3,26 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
-    private static Scanner scanner = new Scanner(System.in);
-
-    public static int inputControl(String player, int limit) {
-        String message;
-        if (limit == 3) {
-            message = player + " can take 1 to 3 matches";
-        } else if (limit == 2) {
-            message = player + " can take 1 or 2 matches";
-        } else {
-            message = player + " can take 1 match";
-        }
-        int matches;
-        do {
-            System.out.println(message);
-            matches = Integer.parseInt(scanner.nextLine());
-        } while (matches < 1 || matches > limit);
-        return matches;
-    }
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("The game 11");
         boolean turn = true;
         int count = 11;
         while (count > 0) {
             System.out.println("On the table are " + count + " matches");
             String player = turn ? "First player" : "Second player";
-            int matches;
-            if (count >= 3) {
-                matches = inputControl(player, 3);
+            if (count > 3) {
+                System.out.println(player + " can take 1 to 3 matches");
             } else if (count == 2) {
-                matches = inputControl(player, 2);
+                System.out.println(player + " can take 1 or 2 matches");
             } else {
-                matches = inputControl(player, 1);
+                System.out.println(player + " can take only 1 matches");
             }
-            count -= matches;
-            turn = !turn;
+            int matches = Integer.parseInt(scanner.nextLine());
+            if (matches >= 1 && matches <= Math.min(3, count)) {
+                count -= matches;
+                turn = !turn;
+            }
         }
         scanner.close();
         if (turn) {

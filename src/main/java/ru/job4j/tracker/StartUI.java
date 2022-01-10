9fd481290036ad1a32cd39c4677cae59cc1ef310
Bolun Andrei <telfagor@gmail.com>
2022-01-10 21:36:59 +0200

    2.7. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -47,6 +47,16 @@ public class StartUI {
                 } else {
                     System.out.println("Item deletion error");
                 }
+            } else if (select == 4) {
+                System.out.println("=== Find item by id ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Item with this ID does not exist");
+                }
             } else if (select == 6) {
                 run = false;
             }

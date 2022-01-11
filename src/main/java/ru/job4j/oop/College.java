c58bff427a2d6fbd3a2d529d00de207fd4bf38e5
Bolun Andrei <telfagor@gmail.com>
2022-01-11 21:52:30 +0200

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+        StudentTwo andrei = new Freshman();
+        Object o = andrei;
+        //Это называется up casting
+    }
+}
